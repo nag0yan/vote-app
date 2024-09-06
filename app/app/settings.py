cc0943d3a05f41458ac815b53f8958b7c97bab85
nag0yan <matsui-ka@kadokawa.jp>
2024-09-07 01:32:21 +0900

    rotate and ignore secret

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bjyz6rcw%#0e1mzi)18%x2h1!%v9ves58&wtq&k8v(tcvs6hr9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
